79a3490516958ec8dd217eee3439a25b5283f708
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-12-11 16:12:47 +0800

    Wrap textarea inside a div (Fix #1)

--- src/components/Input/TextArea/TextArea.js
+++ src/components/Input/TextArea/TextArea.js
@@ -3,13 +3,19 @@ import React from 'react';
 import './TextArea.css';
 
 const TextArea = ({ className = '', name, placeholder, value, onChange }) => (
-  <textarea
-    name={name}
-    className={`input-textarea ${className}`}
-    placeholder={placeholder}
-    value={value}
-    onChange={onChange}
-  />
+  <div>
+    {/* 
+      Wrapping the textarea in a <div> because of a bug in Chrome (on OSX only) that collapses a textarea when text is typed if it's a grid item ...
+      https://github.com/rachelandrew/gridbugs#11-a-textarea-that-is-a-grid-item-collapses-to-zero-width
+    */}
+    <textarea
+      name={name}
+      className={`input-textarea ${className}`}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  </div>
 );
 
 export default TextArea;
